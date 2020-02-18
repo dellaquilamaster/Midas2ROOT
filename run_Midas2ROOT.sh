b229223ba640677f9f7e18cb5c1f44dcd748a152
Daniele Dell'Aquila <daniele@ddellaq.zef.irb.hr>
2020-02-18 15:48:10 +0100

    Improved automatic script

--- run_Midas2ROOT.sh
+++ run_Midas2ROOT.sh
@@ -16,6 +16,10 @@ else
   last_run=$2
 fi
 
+if [ ! -z "$3" ]; then
+  parallel_processes=$3
+fi
+
 for run_num in `seq $first_run $last_run`
 do
   re='^[0-9]+$'
@@ -27,13 +31,29 @@ do
   for file_name in ${DATA_PATH}${RUN_PREFIX}${run_num}_*
   do
   
-    if [ ! -f $file_name ]; then
-      echo "Skipping run $run_num!"
-      continue
-    fi
-    run_name="${file_name##*/$RUN_PREFIX}"
-    echo "Unpacking file $run_name..."
-    eval './exec_Midas2ROOT.exe --run=${run_name}'
-  done
+    if [ -z "$3" ]; then
   
+      if [ ! -f $file_name ]; then
+        echo "Skipping run $run_num!"
+        continue
+      fi
+      run_name="${file_name##*/$RUN_PREFIX}"
+      echo "Unpacking file $run_name..."
+      eval './exec_Midas2ROOT.exe --run=${run_name}'
+      
+    else 
+    
+      if [ ! -f $file_name ]; then
+        echo "echo Skipping run $run_num!" >> parallel_runs.txt
+        continue
+      fi
+      run_name="${file_name##*/$RUN_PREFIX}"
+      echo "echo Unpacking file $run_name... && eval './exec_Midas2ROOT.exe --run=${run_name}'" >> parallel_runs.txt      
+    fi
+  done
 done
+
+if [ ! -z "$3" ]; then
+  parallel $parallel_processes < parallel_runs.txt
+  rm -f parallel_runs.txt
+fi
